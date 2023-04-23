bf845f6ccda96b35226a77ba1daa7681beb5a66a
Matt Brannon <mattbrannon.dev@gmail.com>
2023-04-23 02:14:50 -0500

    update type definition

--- src/types/deepl.ts
+++ src/types/deepl.ts
@@ -1,11 +1,16 @@
 import { TranslatorInput, TranslatorResponse } from "./shared";
 
+type IgnorePatterns = {
+  unicode?: string[];
+  regex?: RegExp[];
+};
+
 export type DeepLConfig = {
   formality?: "default" | "prefer_less" | "prefer_more" | "more" | "less";
-  ignoreRegex?: RegExp[];
   tagHandling?: "xml" | "html";
   splitSentences?: "nonewlines" | 0 | 1;
   source_lang?: string;
+  ignore: IgnorePatterns;
 };
 
 export type DeepLResponse = {
