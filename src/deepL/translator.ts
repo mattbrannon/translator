d2a7a826837d08c7a480599d2d5cb1d8cd32ef09
Matt Brannon <mattbrannon.dev@gmail.com>
2023-04-23 02:10:12 -0500

    get source and target languages

--- src/deepL/translator.ts
+++ src/deepL/translator.ts
@@ -1,10 +1,33 @@
+import { env } from "../env";
+const { deepL } = env;
 import { DeepLTranslator } from "../types/deepl";
 
 export function makeDeepL(): DeepLTranslator {
+  const baseUrl = deepL.endpoint;
+
+  const endpoints = {
+    translate: `/v2/translate`,
+    usage: `/v2/usage`,
+    languages: `/v2/languages`,
+  };
+
+  const headers = {
+    Authorization: `DeepL-Auth-Key ${deepL.apiKey}`,
+    "Content-Type": "application/x-www-form-urlencoded",
+  };
+
+  const getLanguages = async (type: "source" | "target") => {
+    const endpoint = `${endpoints.languages}?type=${type}`;
+    const url = new URL(endpoint, baseUrl);
+    const response = await fetch(url, { headers });
+    const json = await response.json();
+    return json;
+  };
+
   return {
     name: "deepL",
-    sourceLanguages: async () => {},
-    targetLanguages: async () => {},
+    sourceLanguages: async () => await getLanguages("source"),
+    targetLanguages: async () => await getLanguages("target"),
     async translate({ text, target, source }, config) {},
     async usage() {},
   };
