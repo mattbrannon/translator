cd6e546768ae5c77f6f2578715c4c4e5827727d0
Matt Brannon <mattbrannon.dev@gmail.com>
2023-04-22 22:27:50 -0500

    add method types

--- src/types/deepl.ts
+++ src/types/deepl.ts
@@ -12,12 +12,22 @@ export type DeepLResponse = {
   translations: [{ text: string; detected_source_language: string }];
 };
 
+export type MethodDeepLTranslate = (
+  input: TranslatorInput,
+  config?: DeepLConfig
+) => Promise<TranslatorResponse>;
+
+export type MethodDeepLLanguages = () => Promise<string[]>;
+
+export type MethodDeepLUsage = () => Promise<{
+  character_count: number;
+  character_limit: number;
+}>;
+
 export type DeepLTranslator = {
-  name: string;
-  translate: (
-    input: TranslatorInput,
-    config?: DeepLConfig
-  ) => Promise<TranslatorResponse>;
-  sourceLanguages: () => Promise<string[]>;
-  targetLanguages: () => Promise<string[]>;
+  name: "deepL";
+  translate: MethodDeepLTranslate;
+  sourceLanguages: MethodDeepLLanguages;
+  targetLanguages: MethodDeepLLanguages;
+  usage: MethodDeepLUsage;
 };
