7dda6097ad436b14f14753a7d3c591a16fc51033
Matt Brannon <mattbrannon.dev@gmail.com>
2023-04-29 07:22:37 -0500

    create shared types

---
+++ src/translators/types.ts
@@ -0,0 +1,27 @@
+export namespace Translate {
+  export type IgnorePatterns = {
+    unicode?: string[];
+    regex?: RegExp[];
+  };
+
+  export interface Options {
+    ignore?: IgnorePatterns;
+    join?: boolean;
+  }
+
+  export interface Input {
+    text: string | string[];
+    target: string;
+    source?: string;
+    options?: Options;
+  }
+
+  export type Response = {
+    translation: string;
+    detectedLanguage: string;
+    original?: string;
+    transliteration?: string | null;
+  };
+
+  export type Output = Promise<Response[] | string>;
+}
