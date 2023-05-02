1e56fd5c8bcc02b2f106603632d67e3f953411bd
Matt Brannon <mattbrannon.dev@gmail.com>
2023-05-02 00:42:34 -0500

    refactor google v2 translator

--- src/translators/google/v2.ts
+++ src/translators/google/v2.ts
@@ -1,122 +1,58 @@
 import { env } from "../../env";
-import { Translate } from "../types";
-import { setIgnoreTags, removeTags } from "../utils";
+import { Google } from "./types";
+import {
+  setIgnoreTags,
+  parseResponse,
+  makePostRequester,
+  makeGetRequester,
+} from "../utils";
 
-type DetectInput = {
-  text: string | string[];
-};
+export function makeV2Translator(apiKey?: string) {
+  const secretKey = apiKey || env.google.apiKey;
 
-type GoogleV2Response = {
-  translatedText: string;
-  detectedSourceLanguage: string;
-};
+  if (!secretKey) return;
 
-const parseResponse = async (response: Response) => {
-  const { status, statusText } = response;
-  const json = await response.json();
-
-  if (status >= 200 && status < 300) {
-    return json;
-  }
-  else {
-    const error = new Error(statusText);
-    throw error;
-  }
-};
-
-const sendRequest = async (url: URL, config: RequestInit) => {
-  try {
-    const response = await fetch(url, config);
-    const json = await parseResponse(response);
-    return json;
-  }
-  catch (error) {
-    throw error;
-  }
-};
-
-const sendPostRequest = async (url: URL, body: string) => {
   const headers = new Headers({
     "Content-Type": "application/json",
   });
 
-  const method = "POST";
-  const config = { method, headers, body };
-  const response = await sendRequest(url, config);
+  const sendPostRequest = makePostRequester(headers);
+  const sendGetRequest = makeGetRequester(headers);
 
-  return response;
-};
-
-const sendGetRequest = async (url: URL) => {
-  const method = "GET";
-  const config = { method };
-  const response = await sendRequest(url, config);
-
-  return response;
-};
-
-const getTranslationObject = (
-  translations: GoogleV2Response[],
-  text: string[]
-) => {
-  const result = translations.map((item, index) => {
-    return {
-      translation: removeTags(item.translatedText),
-      original: text[index],
-      detectedLanguage: item.detectedSourceLanguage,
-    };
-  });
-  return result;
-};
-
-const joinTranslations = (translations: GoogleV2Response[]) => {
-  const result = translations
-    .map((item) => removeTags(item.translatedText))
-    .join(" ");
-  return result;
-};
-
-export function makeV2Translator(apiKey?: string) {
   const getUrl = (endpoint: "translate" | "detect" | "languages") => {
-    const baseUrl = env.google.endpoint;
-    const endpoints = {
-      translate: "/language/translate/v2",
-      detect: "/language/translate/v2/detect",
-      languages: "/language/translate/v2/languages",
-    };
+    const baseUrl = env.google.v2.baseUrl;
+    const endpoints = env.google.v2.endpoints;
 
     const url = new URL(endpoints[endpoint], baseUrl);
-    const params = new URLSearchParams({ key: apiKey as string });
+    const params = new URLSearchParams({ key: secretKey });
 
     url.search = params.toString();
 
     return url;
   };
 
+  const translateEndpoint = getUrl("translate");
+  const detectEndpoint = getUrl("detect");
+  const languagesEndpoint = getUrl("languages");
+
   return {
     name: "google",
-    async translate({ text, target, source, options }: Translate.Input) {
-      text = Array.isArray(text) ? text : [text];
 
-      const url = getUrl("translate");
+    async translate({ text, target, source, options }: Google.Input) {
+      text = Array.isArray(text) ? text : [text];
       const textArray = setIgnoreTags(text, options);
 
       const body = JSON.stringify({ q: textArray, target, source });
-      const response = await sendPostRequest(url, body);
-      const translations = response.data.translations;
+      const response = await sendPostRequest(translateEndpoint, body);
 
-      const result = options?.join
-        ? joinTranslations(translations)
-        : getTranslationObject(translations, text);
-
-      return result;
+      const translations = parseResponse.google(response, text, options);
+      return translations;
     },
 
-    async detectLanguage({ text }: DetectInput) {
+    async detectLanguage({ text }: Google.DetectInput) {
       try {
-        const url = getUrl("detect");
         const body = JSON.stringify({ q: text });
-        const response = await sendPostRequest(url, body);
+        const response = await sendPostRequest(detectEndpoint, body);
         return response;
       }
       catch (error) {
@@ -124,15 +60,12 @@ export function makeV2Translator(apiKey?: string) {
       }
     },
 
-    async getLanguages({ target = "en" }) {
-      const url = getUrl("languages");
-      url.searchParams.append("target", target);
-      try {
-        const response = await sendGetRequest(url);
-        const {
-          data: { languages },
-        } = response;
+    async getLanguages(target: string = "en") {
+      languagesEndpoint.searchParams.set("target", target);
 
+      try {
+        const response = await sendGetRequest(languagesEndpoint);
+        const languages = response.data.languages;
         return languages;
       }
       catch (error) {
