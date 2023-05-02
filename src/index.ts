770d5d74d0bc15de72c04864afc54b6617e8fb55
Matt Brannon <mattbrannon.dev@gmail.com>
2023-05-02 01:04:00 -0500

    set google translator to exported object

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,23 @@
-import { makeDeepL } from "./translators/deepL/translator";
+import { makeDeepLTranslator } from "./translators/deepL/translator";
 import { makeMicrosoftTranslator } from "./translators/microsoft/translator";
-import { env } from "./env";
+import { makeGoogleTranslator } from "./translators/google/translator";
+// import { env } from "./env";
+// import type { DeepL } from "./translators/deepL/types";
 
 type ApiKeys = {
   microsoft?: string;
   deepL?: string;
+  google?: string;
 };
 
 export const makeTranslators = (apiKeys: ApiKeys) => {
-  const deepL = makeDeepL(apiKeys.deepL);
+  const deepL = makeDeepLTranslator(apiKeys.deepL);
   const microsoft = makeMicrosoftTranslator(apiKeys.microsoft);
+  const google = makeGoogleTranslator(apiKeys.google)!;
+
   return {
     deepL,
     microsoft,
+    google,
   };
 };
