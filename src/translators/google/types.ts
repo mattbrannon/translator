8765d3149a31c5651f1f4b3c1db276709c650829
Matt Brannon <mattbrannon.dev@gmail.com>
2023-05-02 00:30:28 -0500

    add google translator types

---
+++ src/translators/google/types.ts
@@ -0,0 +1,12 @@
+import { Translate } from "../types";
+
+export namespace Google {
+  export type V1FetchResponse = [[[string, string]], any, string];
+
+  export type DetectInput = {
+    text: string | string[];
+  };
+
+  export type Input = Translate.Input;
+  export type Options = Translate.Options;
+}
