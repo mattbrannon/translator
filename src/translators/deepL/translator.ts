7f3510a22ec826284135fdcd08aa0b2dffbbd801
Matt Brannon <mattbrannon.dev@gmail.com>
2023-05-02 00:54:26 -0500

    refactor deepL translator

--- src/translators/deepL/translator.ts
+++ src/translators/deepL/translator.ts
@@ -1,105 +1,50 @@
 import type { DeepL } from "./types";
-import type { Translate } from "../types";
-import {
-  setSearchParams,
-  setInputText,
-  parseResponse,
-  getResult,
-} from "../utils";
+import { parseResponse, makePostRequester, makeGetRequester } from "../utils";
+import { setRequestBody } from "./helpers";
+import { env } from "../../env";
 
 export function makeDeepL(apiKey?: string) {
-  const baseUrl = "https://api-free.deepl.com";
-  const endpoints = {
-    translate: `/v2/translate`,
-    usage: `/v2/usage`,
-    languages: `/v2/languages`,
-  };
+  const secretKey = apiKey || env.deepL.apiKey;
 
-  const headers = {
-    Authorization: `DeepL-Auth-Key ${apiKey}`,
-    "Content-Type": "application/x-www-form-urlencoded",
-  };
+  if (!secretKey) return;
+
+  const baseUrl = env.deepL.baseUrl;
+  const endpoints = env.deepL.endpoints;
+
+  const headers = new Headers({
+    Authorization: `DeepL-Auth-Key ${secretKey}`,
+    "Content-Type": "application/json",
+  });
+
+  const sendPostRequest = makePostRequester(headers);
+  const sendGetRequest = makeGetRequester(headers);
 
   const getLanguages = async (type: "source" | "target") => {
     const endpoint = `${endpoints.languages}?type=${type}`;
     const url = new URL(endpoint, baseUrl);
-    const response = await fetch(url, { headers });
-    const json = await response.json();
-    return json;
-  };
-
-  const getURL = ({ text, target, source, options }: DeepL.Input) => {
-    const url = new URL(endpoints.translate, baseUrl);
-
-    const hasRegex = () => {
-      return !!(
-        options?.ignore?.regex?.length || options?.ignore?.unicode?.length
-      );
-    };
-
-    const setTags = () => {
-      if (!hasRegex()) return;
-
-      const ignoreTags = Array.isArray(options?.ignoreTags)
-        ? [ ...(options?.ignoreTags as []), "ignore" ]
-        : [ options?.ignoreTags, "ignore" ];
-
-      const tags = [...new Set(ignoreTags.filter((v) => v))];
-      console.log({ tags });
-
-      return tags;
-      // return [...new Set(ignoreTags.filter((v) => v))];
-    };
-
-    const snakeCasedOptions = Object.fromEntries(
-      Object.entries(options || {}).map(([ k, v ]) => [
-        k.replace(/([A-Z])/g, "_$1").toLowerCase(),
-        v,
-      ])
-    );
-
-    const params = setSearchParams({
-      ...snakeCasedOptions,
-      source_lang: source,
-      target_lang: target,
-      tag_handling: "xml",
-      ignore_tags: "ignore",
-      text: setInputText(text, options?.ignore, "deepl"),
-    });
-
-    url.search = params.toString();
-
-    return url;
+    return await sendGetRequest(url);
   };
 
   return {
     name: "deepL",
-    languages: {
-      async source(): Promise<DeepL.LanguageResponse> {
-        return await getLanguages("source");
-      },
-      async target(): Promise<DeepL.LanguageResponse> {
-        return await getLanguages("target");
-      },
+
+    async sourceLanguages() {
+      return await getLanguages("source");
+    },
+    async targetLanguages() {
+      return await getLanguages("target");
     },
 
-    async translate({
-      text,
-      target,
-      source,
-      options,
-    }: DeepL.Input): Translate.Output {
+    async translate(input: DeepL.Input): DeepL.Output {
       try {
-        const method = "POST";
-        const url = getURL({ text, target, source, options });
+        const text = Array.isArray(input.text) ? input.text : [input.text];
+        const body = JSON.stringify(setRequestBody({ ...input, text }));
 
-        const response = await fetch(url, { method, headers, body: null });
+        const url = new URL(endpoints.translate, baseUrl);
+        const response = await sendPostRequest(url, body);
+        const translations = parseResponse.deepL(response, text, input.options);
 
-        const json: DeepL.FetchResponse = await response.json();
-        const translations = parseResponse.deepL(json);
-        const result = getResult(translations, text, options);
-
-        return result;
+        return translations;
       }
       catch (error) {
         const e = error as Error;
@@ -107,29 +52,10 @@ export function makeDeepL(apiKey?: string) {
       }
     },
 
-    /**
-     * Checks the current usage of the DeepL API key.
-     * @async @function
-     * @returns {DeepL.UsageResponse} Returns an object containing the current usage of the DeepL API key.
-     * @throws {Error} Throws an error if there was an issue with the request.
-     *
-     * @example <caption>Get the current usage of the DeepL API key.</caption>
-     * const usage = await deepL.usage();
-     * console.log(usage);
-     * // {
-     * //   character_count: 0,
-     * //   character_limit: 5000000,
-     * // }
-     */
-
     async usage(): DeepL.UsageResponse {
-      const method = "GET";
-      const endpoint = endpoints.usage;
       try {
-        const url = new URL(endpoint, baseUrl);
-        const response = await fetch(url, { method, headers });
-        const json = await response.json();
-        return json;
+        const url = new URL(endpoints.usage, baseUrl);
+        return await sendGetRequest(url);
       }
       catch (error) {
         console.error(error);
@@ -139,98 +65,3 @@ export function makeDeepL(apiKey?: string) {
     },
   };
 }
-
-// type DeepLResponse = {
-//   translations: [{ text: string; detected_source_language: string }];
-// };
-
-// const camelToSnake = (str: string) => {
-//   return str.replace(/[A-Z]/g, (letter) => `_${letter.toLowerCase()}`);
-// };
-
-// const setJsonBody = (options: DeepL.Options) => {
-//   const customKeys = new Set([ "ignore", "join" ]);
-//   const json = Object.keys(options).reduce((body, key) => {
-//     const value = options[key];
-//     if (value !== undefined && !customKeys.has(key)) {
-//       body[camelToSnake(key)] = value;
-//     }
-//     return body;
-//   }, {} as DeepL.RequestBody);
-
-//   return json;
-// };
-
-// const removeExtraSpaces = (text: string[]) => {
-//   return text.map((t) => t.replace(/\s+/g, " ").trim());
-// };
-
-// const setTextArray = (text: string | string[]) => {
-//   return removeExtraSpaces(Array.isArray(text) ? text : [text]);
-// };
-
-// const options: Options = {
-//   target_lang: "EN",
-//   source_lang: "DE",
-//   formality: "prefer_less",
-//   tagHandling: "xml",
-//   splitSentences: "nonewlines",
-//   ignoreTags: ["ignore"],
-//   preserveFormatting: "0",
-// };
-
-// const getRequestBody = ({ text, target, source, options }: DeepL.Input) => {
-//   const json = setJsonBody({
-//     ...options,
-//     source_lang: source,
-//   } as DeepL.Options);
-
-//   const textArray = setInputText(text, options?.ignore);
-//   const body = { ...json, text: textArray, target_lang: target };
-//   return JSON.stringify(body);
-// };
-
-// const setDeepLSearchParams = (options: DeepL.Options) => {
-//   const customKeys = new Set([ "ignore", "join" ]);
-//   const params = new URLSearchParams();
-//   Object.keys(options).forEach((key) => {
-//     const value = options[key];
-//     const k = camelToSnake(key);
-//     if (!customKeys.has(key) && value !== undefined) {
-//       if (Array.isArray(value)) {
-//         value.forEach((item) => {
-//           params.append(k, item);
-//         });
-//       }
-//       else {
-//         params.set(k, value);
-//       }
-//     }
-//   });
-//   return params;
-// };
-
-// const getTranslation = (json: DeepLResponse) => {
-//   const translations = json.translations.map((t) => {
-//     return {
-//       detectedLanguage: t.detected_source_language,
-//       translation: t.text.replace(/<\/?ignore>/g, "").trim(),
-//     };
-//   });
-//   return translations;
-// };
-
-// const getOriginal = (text: string[]) => {
-//   return text.map((t) => t.replace(/<\/?ignore>/g, "").trim());
-// };
-
-// const getTranslationWithOriginal = (json: DeepLResponse, text: string[]) => {
-//   const translations = json.translations.map((t, i) => {
-//     return {
-//       detectedLanguage: t.detected_source_language,
-//       translation: t.text.replace(/<\/?ignore>/g, "").trim(),
-//       original: text[i].replace(/<\/?ignore>/g, "").trim(),
-//     };
-//   });
-//   return translations;
-// };
