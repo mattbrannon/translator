b14688c173d384ac5365192fb468ee942a2e8f98
Matt Brannon <mattbrannon.dev@gmail.com>
2023-05-02 00:38:24 -0500

    update utils

--- src/translators/utils.ts
+++ src/translators/utils.ts
@@ -2,16 +2,11 @@ import { Translate } from "./types";
 import type { DeepL } from "./deepL/types";
 import type { Microsoft } from "./microsoft/types";
 
-const removeExtraSpaces = (text: string[]) => {
-  return text.map((t) => t.replace(/\s+/g, " ").trim());
-};
-
-const setTextArray = (text: string | string[]) => {
-  return removeExtraSpaces(Array.isArray(text) ? text : [text]);
-};
+// const removeExtraSpaces = (text: string[]) => {
+//   return text.map((t) => t.replace(/\s+/g, " ").trim());
+// };
 
 const setIgnoreUnicodes = (unicodes: string[]) => {
-  // [...new Set([ ...unicodes, "Extended_Pictographic" ])]
   return [...new Set([...unicodes])]
     .map((unicode) => {
       try {
@@ -37,11 +32,17 @@ const setIgnoreRegexes = (regexes: RegExp[]) => {
     .filter((v) => v);
 };
 
+const setTextArray = (text: string | string[]) => {
+  text = Array.isArray(text) ? text : [text];
+  const textArray = text.map((str) => str.replace(/\s+/g, " ").trim());
+  return textArray;
+};
+
 export const setIgnoreTags = (
-  text: string[],
-  options?: DeepL.Options | Microsoft.Options
+  text: string | string[],
+  options?: Translate.Options
 ) => {
-  return text.map((str) => {
+  return setTextArray(text).map((str) => {
     const regex = options?.ignore?.regex || [];
     const unicodes = options?.ignore?.unicode || [];
     const expressions = [
@@ -51,9 +52,11 @@ export const setIgnoreTags = (
     expressions.forEach((regex) => {
       const matches = str.match(regex as RegExp);
       matches?.forEach((match) => {
-        const markup = options?.ignoreTags
-          ? `<ignore>${match}</ignore>`
-          : `<div class="notranslate">${match}</div>`;
+        const markup =
+          // @ts-ignore
+          options?.tagHandling === "xml"
+            ? `<ignore>${match}</ignore>`
+            : `<span class="notranslate">${match}</span>`;
         str = str.replace(match, markup);
       });
     });
@@ -61,40 +64,19 @@ export const setIgnoreTags = (
   });
 };
 
-const removeTags = (text: string) => {
+export const removeTags = (text: string) => {
   return text
     ?.replace(/<\/?[^>]+(>|$)/g, " ")
     .replace(/\s+/g, " ")
     .trim();
 };
 
-export const setInputText = (
-  text: string | string[],
-  ignore: Translate.IgnorePatterns = {},
-  name: string
-) => {
-  const textArray = setTextArray(text);
-  const unicodes = ignore?.unicode || [];
-  const regexes = ignore?.regex || [];
-
-  const ignoreUnicodes = setIgnoreUnicodes(unicodes) as RegExp[];
-  const ignoreRegexes = setIgnoreRegexes(regexes) as RegExp[];
-
-  const ignoreTags = [ ...ignoreUnicodes, ...ignoreRegexes ];
-  const inputText = setIgnoreTags(ignoreTags, textArray, name);
-
-  console.log("inputText", inputText);
-
-  return inputText;
-};
-
 export const getRequestBody = (
   text: string | string[],
   options?: Microsoft.Options
 ) => {
-  return setInputText(text, options?.ignore, "microsoft").map((text) => ({
-    text,
-  }));
+  const textArray = setIgnoreTags(text, options).map((text) => ({ text }));
+  return JSON.stringify(textArray);
 };
 
 export const setSearchParams = (options: DeepL.Options | Microsoft.Options) => {
@@ -116,39 +98,136 @@ export const setSearchParams = (options: DeepL.Options | Microsoft.Options) => {
   return params;
 };
 
-const parseMicrosoftResponse = (json: Microsoft.FetchResponse) => {
+type GoogleV2Response = {
+  data: {
+    translations: {
+      translatedText: string;
+      detectedSourceLanguage: string;
+    }[];
+  };
+};
+
+const joinTranslations = (translations: Translate.Response[]) => {
+  const result = translations
+    .map((item) => removeTags(item.translation))
+    .join(" ");
+  return result;
+};
+
+const formatMicrosoftResponse = (
+  json: Microsoft.FetchResponse,
+  text: string[]
+) => {
   return json.map((item: any, i: number) => {
     return {
       translation: removeTags(item.translations[0].text),
       transliteration:
         removeTags(item.translations[0]?.transliteration?.text) || null,
       detectedLanguage: item.detectedLanguage.language as string,
-      original: "",
+      original: text[i],
     };
   });
 };
 
-const parseDeepLResponse = (json: DeepL.FetchResponse) => {
+const parseMicrosoftResponse = (
+  json: Microsoft.FetchResponse,
+  text: string[],
+  options?: Translate.Options
+) => {
+  const translations = formatMicrosoftResponse(json, text);
+  return options?.join ? joinTranslations(translations) : translations;
+};
+
+export const formatDeepLResponse = (
+  json: DeepL.FetchResponse,
+  text: string[]
+) => {
   const translations = json.translations.map((t, i) => {
     return {
       detectedLanguage: t.detected_source_language,
-      translation: t.text.replace(/<\/?ignore>/g, "").trim(),
-      original: "",
+      translation: removeTags(t.text), //replace(/<\/?ignore>/g, "").trim(),
+      original: text[i],
     };
   });
   return translations;
 };
 
+const parseDeepLResponse = (
+  json: DeepL.FetchResponse,
+  text: string[],
+  options?: Translate.Options
+) => {
+  const translations = formatDeepLResponse(json, text);
+  return options?.join ? joinTranslations(translations) : translations;
+};
+
+const formatGoogleResponse = (json: GoogleV2Response, text: string[]) => {
+  return json.data.translations.map((item, index) => {
+    return {
+      translation: removeTags(item.translatedText),
+      original: text[index],
+      detectedLanguage: item.detectedSourceLanguage,
+    };
+  });
+};
+
+const parseGoogleResponse = (
+  json: GoogleV2Response,
+  text: string[],
+  options?: Translate.Options
+) => {
+  const translations = formatGoogleResponse(json, text);
+  return options?.join ? joinTranslations(translations) : translations;
+};
+
 export const parseResponse = {
-  deepL(json: DeepL.FetchResponse) {
-    const translations = parseDeepLResponse(json);
+  deepL(
+    json: DeepL.FetchResponse,
+    text: string[],
+    options?: Translate.Options
+  ) {
+    const translations = parseDeepLResponse(json, text, options);
     return translations;
   },
-  microsoft(json: Microsoft.FetchResponse) {
-    const translations = parseMicrosoftResponse(json);
+  microsoft(
+    json: Microsoft.FetchResponse,
+    text: string[],
+    options?: Translate.Options
+  ) {
+    const translations = parseMicrosoftResponse(json, text, options);
+    return translations;
+  },
+  google(json: GoogleV2Response, text: string[], options?: Translate.Options) {
+    const translations = parseGoogleResponse(json, text, options);
     return translations;
   },
 };
+
+export const formatInputText = (text: string | string[]) => {
+  const textArray = Array.isArray(text) ? text : [text];
+
+  return {
+    google: () => textArray,
+    deepL: () => ({ text: textArray }),
+    microsoft: () => textArray.map((text) => ({ text })),
+  };
+};
+
+export const makeTextFormatter =
+  (name: string) =>
+    (options: Translate.Options) =>
+      (text: string | string[]) => {
+        const textArray = Array.isArray(text) ? text : [text];
+        const taggedText = setIgnoreTags(textArray, options);
+        const getInputText = formatInputText(taggedText);
+
+        const inputText = getInputText[name as Translate.ServiceName]();
+        return inputText;
+      };
+
+export const makeDeepLFormatter = makeTextFormatter("deepL");
+export const makeGoogleFormatter = makeTextFormatter("google");
+export const makeMicrosoftFormatter = makeTextFormatter("microsoft");
 
 export function createLanguageArray(obj: Microsoft.Language.Response) {
   const languageArray = [];
@@ -172,19 +251,54 @@ export function createLanguageArray(obj: Microsoft.Language.Response) {
   return languageArray;
 }
 
-export const getResult = async (
-  translations: Translate.Response[],
-  text: string | string[],
-  options?: DeepL.Options | Microsoft.Options
-) => {
-  const originalText = Array.isArray(text) ? text : [text];
+const parseFetchResponse = async (response: Response) => {
+  const { status, statusText } = response;
+  const json = await response.json();
 
-  const result = options?.join
-    ? translations.map((t) => t.translation).join(" ")
-    : translations.map((translation, i) => {
-      translation.original = originalText[i];
-      return translation;
-    });
+  if (status >= 200 && status < 300) {
+    return json;
+  }
+  else {
+    const error = new Error(statusText);
+    throw error;
+  }
+};
 
-  return result;
+const sendRequest = async (url: URL, config: RequestInit) => {
+  try {
+    const response = await fetch(url, config);
+    const json = await parseFetchResponse(response);
+    return json;
+  }
+  catch (error) {
+    throw error;
+  }
+};
+
+export const sendGetRequest = async (url: URL) => {
+  const method = "GET";
+  const config = { method };
+  const response = await sendRequest(url, config);
+
+  return response;
+};
+
+export const makePostRequester = (headers: Headers) => {
+  return async (url: URL, body: string) => {
+    const method = "POST";
+    const config = { method, headers, body };
+    const response = await sendRequest(url, config);
+
+    return response;
+  };
+};
+
+export const makeGetRequester = (headers: Headers) => {
+  return async (url: URL) => {
+    const method = "GET";
+    const config = { method, headers };
+    const response = await sendRequest(url, config);
+
+    return response;
+  };
 };
